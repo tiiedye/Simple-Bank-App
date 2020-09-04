468097038f598c327aedbf9fb90486936789ac04
tiiedye <tiiedye@gmail.com>
2020-09-04 10:30:44 -0700

    updating Account Utils header and source files

--- testing/testing/Account_Util.h
+++ testing/testing/Account_Util.h
@@ -10,10 +10,9 @@ void display(const std::vector<Account>& accounts);
 void deposit(std::vector<Account>& accounts, double amount);
 void withdraw(std::vector<Account>& accounts, double amount);
 
-// Utility helper functions for Savings_Account class
+// Utility helper functions for Savings Account class
 
 void display(const std::vector<Savings_Account>& accounts);
 void deposit(std::vector<Savings_Account>& accounts, double amount);
 void withdraw(std::vector<Savings_Account>& accounts, double amount);
-
-#endif // !_ACCOUNT_UTIL_H_
+#endif
