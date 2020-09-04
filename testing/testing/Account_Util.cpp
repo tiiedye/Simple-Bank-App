82cc2f72e09a92fbc1db090fbbfc6d4ede718f9d
tiiedye <tiiedye@gmail.com>
2020-09-04 10:40:26 -0700

    organizing files

--- testing/testing/Account_Util.cpp
+++ testing/testing/Account_Util.cpp
@@ -3,14 +3,15 @@
 
 // Displays Account objects in a  vector of Account objects 
 void display(const std::vector<Account>& accounts) {
-    std::cout << "\n=== Accounts ==========================================" << std::endl;
-    for (const auto& acc : accounts)
+    std::cout << "\n============= Accounts ==============" << std::endl;
+    for (const auto& acc : accounts) {
         std::cout << acc << std::endl;
+    }
 }
 
 // Deposits supplied amount to each Account object in the vector
 void deposit(std::vector<Account>& accounts, double amount) {
-    std::cout << "\n=== Depositing to Accounts =================================" << std::endl;
+    std::cout << "\n========== Depositing to Accounts ==========" << std::endl;
     for (auto& acc : accounts) {
         if (acc.deposit(amount)) {
             std::cout << "Deposited " << amount << " to " << acc << std::endl;
@@ -22,7 +23,7 @@ void deposit(std::vector<Account>& accounts, double amount) {
 
 // Withdraw amount from each Account object in the vector
 void withdraw(std::vector<Account>& accounts, double amount) {
-    std::cout << "\n=== Withdrawing from Accounts ==============================" << std::endl;
+    std::cout << "\n========== Withdrawing from Accounts ==========" << std::endl;
     for (auto& acc : accounts) {
         if (acc.withdraw(amount)) {
             std::cout << "Withdrew " << amount << " from " << acc << std::endl;
@@ -37,14 +38,15 @@ void withdraw(std::vector<Account>& accounts, double amount) {
 
 // Displays Savings Account objects in a  vector of Savings Account objects 
 void display(const std::vector<Savings_Account>& accounts) {
-    std::cout << "\n=== Savings Accounts ====================================" << std::endl;
-    for (const auto& acc : accounts)
+    std::cout << "\n========== Savings Accounts ==========" << std::endl;
+    for (const auto& acc : accounts) {
         std::cout << acc << std::endl;
+    }
 }
 
 // Deposits supplied amount to each Savings Account object in the vector
 void deposit(std::vector<Savings_Account>& accounts, double amount) {
-    std::cout << "\n=== Depositing to Savings Accounts ==========================" << std::endl;
+    std::cout << "\n========== Depositing to Savings Accounts ==========" << std::endl;
     for (auto& acc : accounts) {
         if (acc.deposit(amount)) {
             std::cout << "Deposited " << amount << " to " << acc << std::endl;
@@ -56,7 +58,7 @@ void deposit(std::vector<Savings_Account>& accounts, double amount) {
 
 // Withdraw supplied amount from each Savings Account object in the vector
 void withdraw(std::vector<Savings_Account>& accounts, double amount) {
-    std::cout << "\n=== Withdrawing from Savings Accounts ======================" << std::endl;
+    std::cout << "\n==========                                                                  Withdrawing from Savings Accounts ==========" << std::endl;
     for (auto& acc : accounts) {
         if (acc.withdraw(amount)) {
             std::cout << "Withdrew " << amount << " from " << acc << std::endl;
